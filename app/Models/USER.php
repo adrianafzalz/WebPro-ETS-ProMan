c1917b7f0495ffcdfc630ca266af55807ce0781b
aaza <dump.truck.aja@gmail.com>
2025-10-22 20:02:47 +0700

    fix:vercel: User to USER

--- app/Models/USER.php
+++ app/Models/USER.php
@@ -8,7 +8,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\USER as Authenticatable;
 
 
 /**
