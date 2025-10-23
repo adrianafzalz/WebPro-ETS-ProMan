110b9ec16291dc32b222c8f5a53e8b17c7eb2148
aaza <dump.truck.aja@gmail.com>
2025-10-23 18:53:01 +0700

    try one

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+// app/Providers/AppServiceProvider.php
+
+use Illuminate\Support\Facades\URL; 
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,11 +19,16 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        // Check if the application is running in a production environment
+        if ($this->app->environment('production', 'staging')) {
+            // Force all generated URLs to use the HTTPS scheme
+            URL::forceScheme('https'); 
+        }
     }
 }
